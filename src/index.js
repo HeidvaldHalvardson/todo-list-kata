4c4bbd9129047d9b682b58bd0831f7308f7b1d04
Vlad Bogodyazh <liquid98@yandex.by>
2023-12-19 22:08:09 +0300

    TODO App #5 - Tooling и развертывание приложения

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 
 import App from './components/App/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+console.log(123)
