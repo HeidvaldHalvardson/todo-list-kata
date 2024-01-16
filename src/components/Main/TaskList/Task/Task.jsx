81344571767eb6e9cc96b710d81047861f8272a9
Vlad Bogodyazh <liquid98@yandex.by>
2024-01-16 11:47:50 +0300

    TODO App с таймером

--- src/components/Main/TaskList/Task/Task.jsx
+++ src/components/Main/TaskList/Task/Task.jsx
@@ -5,7 +5,54 @@ import PropTypes from 'prop-types'
 import DateCreated from './DateCreated/DateCreated'
 
 export default class Task extends React.Component {
+  state = {
+    minutes: 0,
+    seconds: 0,
+    isTimerStart: false,
+  }
+
+  setTimer = () => {
+    let minutes = this.state.minutes
+    let seconds = this.state.seconds
+
+    seconds++
+
+    if (seconds > 59) {
+      minutes++
+      seconds = 0
+    }
+
+    this.setState({
+      minutes,
+      seconds,
+    })
+  }
+
+  startTimer = () => {
+    if (this.props.props.active && !this.state.isTimerStart) {
+      this.setState({
+        isTimerStart: true,
+      })
+      this.timer = setInterval(() => {
+        this.setTimer()
+      }, 1000)
+    }
+  }
+
+  pauseTimer = () => {
+    clearInterval(this.timer)
+    this.setState({
+      isTimerStart: false,
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
   render() {
+    const minutes = this.state.minutes < 10 ? `0${this.state.minutes}` : this.state.minutes
+    const seconds = this.state.seconds < 10 ? `0${this.state.seconds}` : this.state.seconds
     const { props, onDeleted, onLabelClick } = this.props
     return (
       <li key={props.id} className={`todo-item ${!props.active ? 'completed' : ''} ${props.edit ? 'editing' : ''}`}>
@@ -14,10 +61,17 @@ export default class Task extends React.Component {
         ) : (
           <div className="view">
             <input className="toggle" type="checkbox" onClick={onLabelClick} />
-            <label onClick={onLabelClick}>
-              <span className="description">{props.text}</span>
+            <div className="task-description">
+              <label onClick={onLabelClick}>
+                <span className="description">{props.text}</span>
+              </label>
+              <div className="task-timer">
+                <button onClick={this.startTimer} className="icon icon-play" />
+                <button onClick={this.pauseTimer} className="icon icon-pause" />
+                <span>{`${minutes}:${seconds}`}</span>
+              </div>
               <DateCreated />
-            </label>
+            </div>
             <button className="icon icon-edit" />
             <button className="icon icon-destroy" onClick={onDeleted} />
           </div>
